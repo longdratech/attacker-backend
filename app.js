0f8ca87d7ca700bc2855ebe58e7fb744431b083d
Long handsome <>
2020-06-21 18:42:04 +0700

    url

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
-app.get('/', function(req, res) {
+const url = 'https://attacker-kma.herokuapp.com/';
+app.get('https://attacker-kma.herokuapp.com/', function(req, res) {
   res.send('hello world');
 });
 
